bd395041181a7ba46608d8a6527b7b4307e5c858
kjy0349 <kjy0349@naver.com>
2023-11-13 13:50:58 +0900

    port changed

--- src/main/java/com/tripinfo/EnjoyTripFrameworkSeoul9KjyKyuApplication.java
+++ src/main/java/com/tripinfo/EnjoyTripFrameworkSeoul9KjyKyuApplication.java
@@ -9,5 +9,4 @@ public class EnjoyTripFrameworkSeoul9KjyKyuApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnjoyTripFrameworkSeoul9KjyKyuApplication.class, args);
     }
-
 }
